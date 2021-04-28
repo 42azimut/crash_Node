d962d4db1f1b43f1785d029be4e06da14a7274d1
42azimut <azimutuniverse@gmail.com>
2021-04-29 00:09:57 +0900

    5.npm 6.express done

--- crashCourse/04_req_res.js/req_res.js
+++ crashCourse/04_req_res.js/req_res.js
@@ -5,14 +5,18 @@ const server = http.createServer((req, res) => {
   console.log(req.url);
   console.log(req.method);
   //res.setHeader('Content-Type', 'text/plain');  //html 태그도 일반 텍스트처리
-  res.setHeader('Content-Type', 'text/html');
-  
-  res.write('hello jayden');  //plain text
-  res.write('<h2>hello jayden</h2>');  //html 
-  res.end('<p> hello server </p>'); 
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('hello jayden');  //plain text
+      res.write('<h2>hello jayden</h2>');  //html 
+      res.end('<p> hello server </p>'); 
+    } else if (req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<p> about route</p>');
+    }
+  }
 });
-
-
 
 server.listen(3000, 'localhost', () => {
   console.log('listening for requests on port 3000');
