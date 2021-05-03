5ead669a1a5cd4b326d3add768900390fd4f85a3
42azimut <azimutuniverse@gmail.com>
2021-05-03 22:17:57 +0900

    55:00 Save Google Profile Data is done

--- nodeScratch/routes/auth.js
+++ nodeScratch/routes/auth.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const passport = require('passport');
-//const { model } = require('mongoose');
-const router = express.Router();
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
 
-// @desc  Auth with Google
-// @route GET/auth/google
+// @desc    Auth with Google
+// @route   GET /auth/google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-// desc Google auth callback
-// GET /auth/google/callback
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
 router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
@@ -17,5 +16,11 @@ router.get(
   }
 )
 
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
-module.exports = router;
+module.exports = router
