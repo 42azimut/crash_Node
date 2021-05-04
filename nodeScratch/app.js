fcfedb3c7d248d3f68af2109c3b74393d4b0869a
42azimut <azimutuniverse@gmail.com>
2021-05-04 18:40:30 +0900

    1:54 Edit story & Trucante & Strip Tags, Edit icon helper

--- nodeScratch/app.js
+++ nodeScratch/app.js
@@ -31,13 +31,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs');
 
 // Handlebars
 app.engine('.hbs', 
   exphbs({ 
     helpers: {
       formatDate,
+      stripTags,
+      truncate,
+      editIcon,
     },
     defaultLayout: 'main', 
     extname: '.hbs'}));
@@ -56,6 +59,12 @@ app.use(
 //Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set glovbal var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next();
+})
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
