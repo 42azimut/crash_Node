6a1515fc0859913b6bebeab890a134ec119a170d
42azimut <azimutuniverse@gmail.com>
2021-05-12 01:42:27 +0900

    show new router

--- nodeMkDnBlog/app.js
+++ nodeMkDnBlog/app.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const app = express()
+const mongoose = require('mongoose');
+const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
+const app = express()
+
+mongoose.connect('mongodb+srv://azimutuniverse:1234@cluster0.20jsw.mongodb.net/nodeblog?retryWrites=true&w=majority', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  })
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)   // :3000/articles/post, edit ...
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  const articles = [{
-    title: 'Test Article',
-    createdAt: new Date(),
-    description: 'Test Des blar bla '
-  },
-  {
-    title: 'Test Article 22',
-    createdAt: new Date(),
-    description: 'Test Des 2222 blar bla '
-  }]
-  res.render('index', { articles: articles })
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)   // :3000/articles/post, edit ...
 
 app.listen(3000)
