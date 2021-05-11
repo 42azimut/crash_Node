713a8a75d5694f2e3695dcd949208e7436622130
42azimut <azimutuniverse@gmail.com>
2021-05-11 23:13:29 +0900

    index.ejs toLocaleDateString() and articles.forEach()

--- nodeMkDnBlog/app.js
+++ nodeMkDnBlog/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express()
+const articleRouter = require('./routes/articles');
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)   // :3000/articles/post, edit ...
 
 app.get('/', (req, res) => {
-  res.send('hi there node blog')
+  const articles = [{
+    title: 'Test Article',
+    createdAt: new Date(),
+    description: 'Test Des blar bla '
+  },
+  {
+    title: 'Test Article 22',
+    createdAt: new Date(),
+    description: 'Test Des 2222 blar bla '
+  }]
+  res.render('index', { articles: articles })
 })
 
 app.listen(3000)
